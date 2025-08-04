a776101156d34e1893b21b342ca304c911b62394
Dlejarza1 <dlejarzan@gmail.com>
2025-08-04 01:48:46 -0600

    Ejemplo asignacion de variables.

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -11,7 +11,39 @@ namespace Calculadora
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Este es un mensaje...");
+            string nombre;
+            string apellido;
+
+            int numeroEntero=5;
+            double numeroDecimal=2.5;
+            double suma = 0;
+            double resta = 0;
+            double multiplicacion = 0;
+            double division = 0;
+
+
+            suma = numeroEntero + numeroDecimal;
+            resta = numeroEntero - numeroDecimal;
+            multiplicacion = numeroEntero * numeroDecimal;
+            division = numeroEntero / numeroDecimal;
+
+            double div1 = 5 / 2;
+            double div2 = 5.0 / 2.0;
+            double div3 = 5.0 / 2;
+
+            Console.WriteLine($"Resultados {div1} {div2} {div3}");
+            /*
+            nombre = "Jose Quezada";
+            apellido = "Morazan Quezada";
+
+            Console.WriteLine("Nombre completo: " + nombre + " " + apellido);
+
+            Console.WriteLine($"Nombre {nombre} {apellido}");
+            */
+            Console.WriteLine($"Resultado de la suma es {suma}");
+            Console.WriteLine($"Resultado de la resta es {resta}");
+            Console.WriteLine($"Resultado de la multiplicacion es {multiplicacion}");
+            Console.WriteLine($"Resultado de la division es {division}");
 
             Console.ReadLine();
 
